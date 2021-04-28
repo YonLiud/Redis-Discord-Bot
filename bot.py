51ad9ea1fd9c82145304b40b91b1c3e176f1576b
YonLiud <ejliud@gmail.com>
2021-04-28 16:37:04 +0300

    add slash commands

--- bot.py
+++ bot.py
@@ -1,11 +1,33 @@
 # bot.py
 from dotenv import dotenv_values
+import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
 
-bot = commands.Bot(command_prefix="prefix")
-slash = SlashCommand(bot, override_type=True)
+bot = commands.Bot(command_prefix="redis ")
+slash = SlashCommand(bot, override_type=True, sync_commands = True)
 
 
-bot.load_extension("cog")
+
+
+@bot.command()
+async def fetch(ctx, args = None):
+    result = discord.Embed(
+        title='Fetch:',
+        description='<key>: <value>',
+        colour=discord.Colour.red()
+    )
+    await ctx.channel.send(embed=result)
+
+
+@slash.slash(name="fetch", guild_ids=[428489090162491392], )
+async def _fetch(ctx, args = None):
+    result = discord.Embed(
+        title='Fetch:',
+        description='<key>: <value>',
+        colour=discord.Colour.red()
+    )
+    await ctx.channel.send(embed=result)
+
+
 bot.run(str(dotenv_values("token.env")["TOKEN"]))
