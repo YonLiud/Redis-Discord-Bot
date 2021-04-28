5d20d5318060578e47007a21a7be93bc665de675
YonLiud <ejliud@gmail.com>
2021-04-29 01:11:47 +0300

    finish day 1

--- bot.py
+++ bot.py
@@ -1,88 +1,114 @@
+import os
+import re
+import sys
+from datetime import datetime
 
-from dotenv import dotenv_values
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option, create_choice
-from datetime import datetime
-import re
+from dotenv import dotenv_values
 
-
-now = datetime.now() # current date and time
+now = datetime.now()  # current date and time
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-
+whitelisted = dotenv_values("config.env")["whitelisted"].split(",")
 bot = commands.Bot(command_prefix="redis ")
-slash = SlashCommand(bot, override_type=True, sync_commands = True)
+slash = SlashCommand(bot, override_type=True, sync_commands=True)
 
-guild_ids=[428489090162491392]
-
-# @bot.command()
-# async def fetch(ctx, args = None):
-#     result = discord.Embed(
-#         title='Fetch:',
-#         description='<key>: <value>',
-#         colour=discord.Colour.red()
-#     )
-#     await ctx.channel.send(embed=result)
+guild_ids = [int(dotenv_values("config.env")["guild_id"])]
 
 
-@slash.slash(name="config", description="Edit `config.env` file",  guild_ids=guild_ids, options=[
-                create_option(
-                 name="option",
-                 description="Ed",
-                 option_type=3,
-                 required=True,
+@slash.slash(name="Reboot", description=f"", guild_ids=guild_ids, )
+async def _reboot(ctx):
+    now = datetime.now()  # current date and time
+    result = discord.Embed(
+        title='Rebooting',
+        description=f'Exec time: {now}\n\n**REBOOTING**',
+        colour=discord.Colour.teal()
+    )
+    try:
+        if ctx.author.id not in whitelisted:
+            raise Exception("01 Permission Denied! User id is not found in whitelisted")
+        await ctx.send(embed=result)
+    except Exception as e:
+        output = f"**An error has occurred**! Exception: ``{e}``"
+        if str(e).startswith("01 "):
+            output += "\n***🔴🔴This incident will be reported!🔴🔴***"
+    else:
+        os.execv(sys.argv[0], sys.argv)
 
-                 choices=[
-                    create_choice(
-                        name="Hostname",
-                        value="hostname"
-                    ),
-                    create_choice(
-                        name="Port",
-                        value="port"
-                    ),
-                     create_choice(
-                         name="Database",
-                         value="db"
-                    )
 
-                 ])
-             ])
+@slash.slash(name="config", description="Edit `config.env` file", guild_ids=guild_ids, options=[
+    create_option(
+        name="option",
+        description="Ed",
+        option_type=3,
+        required=True,
+
+        choices=[
+            create_choice(
+                name="Hostname",
+                value="HOSTNAME"
+            ),
+            create_choice(
+                name="Port",
+                value="PORT"
+            ),
+            create_choice(
+                name="Database",
+                value="DB"
+            )
+
+        ])
+])
 async def _config(ctx, option):
-    msg = ""
+    # msg = ""
     def check_int(m):
         try:
-            val = int(m.content)
+            int(m.content)
             return True
         except ValueError:
             return False
+
     def check_ip(m):
         regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
         x = (re.search(regex, m.content))
         return x
-    await ctx.send(f"Chosen: {option}")
+
     try:
-        if option == "db":
+        if option == "DB":
             await ctx.send(f"Select new database index (MUST BE NUMBER)")
             msg = await bot.wait_for("message")
+            await ctx.message.delete()
             if not (check_int(msg)):
-                raise Exception("Invalid Input")
-        elif option == "port":
+                raise Exception("00 Invalid Input")
+        elif option == "PORT":
             await ctx.send(f"Select new port for Redis-bot (MUST BE NUMBER)")
             msg = await bot.wait_for("message")
+            await ctx.message.delete()
             if not (check_int(msg)):
-                raise Exception("Invalid Input")
-        elif option == "hostname":
+                raise Exception("00 Invalid Input")
+        elif option == "HOSTNAME":
             await ctx.send(f"Select new IP Address for Redis-bot (MUST BE VALID IP)")
             msg = await bot.wait_for("message")
+            await ctx.message.delete()
             if not (check_ip(msg)):
-                raise Exception("Invalid Input")
-        await ctx.send(f"Changing `{option}` to `{msg.content}`")
+                raise Exception("00 Invalid Input")
+        await ctx.send(f"Verifying Permissions for `{ctx.author.id}`...")
+        if ctx.author.id not in whitelisted:
+            raise Exception("01 Permission Denied! User id is not found in whitelisted")
     except Exception as e:
-        await ctx.send(f"**An error has occurred**! Exception: ``{e}``")
+        output = f"**An error has occurred**! Exception: ``{e}``"
 
+        if str(e).startswith("01 "):
+            output += "\n***🔴🔴This incident will be reported!🔴🔴***"
+        elif str(e).startswith("00 "):
+            output += "\n*This incident will **NOT** be reported!*"
+        await ctx.send(output)
+
+    else:
+        await ctx.send("Success!")
 
 
 @slash.slash(name="set", description="Execute `set`", guild_ids=guild_ids, )
@@ -114,5 +140,6 @@ async def on_ready():
     print(f"Bot | ID:         {format(bot.user.id)}")
     print(f"Bot | Name:       {format(bot.user.name)}")
     print(f"Bot is ready to use")
+
 
 bot.run(str(dotenv_values("token.env")["TOKEN"]))
